a54b724d91dc9032a9ac00b3529d2461268a6d54
Your Name <you@example.com>
2022-12-22 16:14:20 -0500

    Got rid of makefile

---
+++ synth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+mkdir -p ./build/
+
+function synth () {
+	# Synthesize with yosys
+	yosys -s ./scripts/yosys/$1.ys 
+
+	# Convert to netlist
+	# cd ./build	&& mknl --input ./$@.json --output ./$@
+}
+
+if [ -n "$1" ]
+then
+    synth $1
+else
+    files=$(find ./src/verilog/ -maxdepth 1 -name *.v)
+    for file in $files 
+    do
+        base=$(basename $file .v)
+        synth $base
+    done
+fi
---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+mkdir -p ./build/
+
+function test () {
+	# Compile with verilog
+	# iverilog -g2012 ./src/verilog/$1.v -o ./build/$1.vpp
+
+	# Run unit test
+	iverilog -g2012 ./src/verilog/$1.v ./lib/macros.v ./tests/"$1"_TB.v -o ./build/$1.vpp
+	./build/$1.vpp
+}
+
+if [ -n "$1" ]
+then
+    test $1
+else
+    files=$(find ./src/verilog/ -maxdepth 1 -name *.v)
+    for file in $files 
+    do
+        base=$(basename $file .v)
+        test $base
+    done
+fi
+  
